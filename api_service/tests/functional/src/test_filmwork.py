9ff6b7f0927a51f89f5385f8a26bc16c0feb79cb
Natalia <maltseva@arrival.com>
2021-05-02 00:22:01 +0300

    Аргумент теста изменен на ожидаемые данные вместо файла, добавлен параметр жанр

--- api_service/tests/functional/src/test_filmwork.py
+++ api_service/tests/functional/src/test_filmwork.py
@@ -1,17 +1,16 @@
-import json
-
 import pytest
 
-from testdata.test_parameters.film_list_params import default_params, sort_params, page_num_params
+from testdata.test_parameters.film_list_params import default_params, sort_params, page_num_params, genre_params
 
 
-@pytest.mark.parametrize("query_params, expected_data_file, status, page_size",
+@pytest.mark.parametrize("query_params, expected_data, status, page_size",
                          [*default_params,
                           *sort_params,
-                          *page_num_params
+                          *page_num_params,
+                          *genre_params
                           ])
 @pytest.mark.asyncio
-async def test_film_list(make_get_request, query_params, expected_data_file, status, page_size):
+async def test_film_list(make_get_request, query_params, expected_data, status, page_size):
     """
     Проверка запроса списка фильмов с передачей параметров
     """
@@ -20,10 +19,8 @@ async def test_film_list(make_get_request, query_params, expected_data_file, sta
     # Проверка статуса ответа
     assert response.status == status
 
-    if expected_data_file:
+    if expected_data is not None:
         # загрузка ожиадемого результата из файла и проверка
         # соответствия ответа содержимому файла
-        with open(expected_data_file, encoding="utf-8") as json_file:
-            expected = json.load(json_file)
         assert len(response.body) == page_size
-        assert response.body == expected
+        assert response.body == expected_data
--- api_service/tests/functional/testdata/test_parameters/film_list_params.py
+++ api_service/tests/functional/testdata/test_parameters/film_list_params.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from pathlib import Path
@@ -5,18 +7,30 @@ from pathlib import Path
 parent_dir = Path(__file__).parents[1]
 files_dir = parent_dir.joinpath("expected_data", "films")
 
-# Format of parameters: (query_params, expected_data_file, status, page_size)
-default_params = [({}, files_dir.joinpath("film_list_default.json"), 200, 50)]
 
-sort_params = [({"sort": "imdb_rating"}, files_dir.joinpath("film_list_default.json"), 200, 50),
-               ({"sort": "-imdb_rating"}, files_dir.joinpath("film_list_desc_sorted.json"), 200, 50),
+def get_data_from_file(expected_data_file):
+    file = files_dir.joinpath(expected_data_file)
+    with open(file, encoding="utf-8") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+# Format of parameters: (query_params, expected_data_list, response_status, page_size)
+default_params = [({}, get_data_from_file("film_list_default.json"), 200, 50)]
+
+sort_params = [({"sort": "imdb_rating"}, get_data_from_file("film_list_default.json"), 200, 50),
+               ({"sort": "-imdb_rating"}, get_data_from_file("film_list_desc_sorted.json"), 200, 50),
                ({"sort": "rating"}, None, 422, None)]
 
-page_num_params = [({"page_number": 0}, files_dir.joinpath("film_list_default.json"), 200, 50),
-                   ({"page_number": 15}, files_dir.joinpath("film_list_page_number_15.json"), 200, 50),
-                   ({"page_number": 19}, files_dir.joinpath("film_list_page_number_19.json"), 200, 49),
+page_num_params = [({"page_number": 0}, get_data_from_file("film_list_default.json"), 200, 50),
+                   ({"page_number": 15}, get_data_from_file("film_list_page_number_15.json"), 200, 50),
+                   ({"page_number": 19}, get_data_from_file("film_list_page_number_19.json"), 200, 49),
                    ({"page_number": "two"}, None, 422, None),
                    pytest.param({"page_number": 20}, None, 204, None,
-                                marks=pytest.mark.xfail(reason="Fix response status 200->204")),
+                                marks=pytest.mark.skip(reason="Fix response status 200->204")),
                    pytest.param({"page_number": -1}, None, 422, None,
-                                marks=pytest.mark.xfail(reason="Fix response status 500->422"))]
+                                marks=pytest.mark.skip(reason="ES throws exception. Fix response status 500->422"))]
+
+genre_params = [({"genre": "Documentary"}, get_data_from_file("film_list_genre_documentary.json"), 200, 50),
+                ({"genre": "Reality-TV"}, get_data_from_file("film_list_genre_realitytv.json"), 200, 38),
+                ({"genre": "Cartoon"}, [], 200, 0)]
