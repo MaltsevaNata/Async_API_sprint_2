87dfb5550cd638ef1755d386db74661294a2c633
Natalia <maltseva@arrival.com>
2021-05-02 23:51:47 +0300

    Описание тестов и передача названия файла в качестве аргумента

--- api_service/tests/functional/src/test_filmwork.py
+++ api_service/tests/functional/src/test_filmwork.py
@@ -1,12 +1,16 @@
-from typing import Union
+from pathlib import Path
 
 import pytest
 
-from testdata.test_parameters.film_list_params import default_params, sort_params, page_num_params, page_size_params
-from testdata.test_parameters.film_list_params import film_id_params, genre_params, combined_params
+from testdata.test_parameters.film_params import default_params, sort_params, page_num_params, page_size_params,\
+                                                 film_id_params, genre_params, combined_params
+from utils.get_data_from_file import get_data_from_file
+
+parent_dir = Path(__file__).parents[1]
+files_dir = parent_dir.joinpath("testdata", "expected_data", "films")
 
 
-@pytest.mark.parametrize("query_params, expected_data, status, page_size",
+@pytest.mark.parametrize("query_params, expected_data_file, status, page_size",
                          [*default_params,
                           *sort_params,
                           *page_num_params,
@@ -15,7 +19,7 @@ from testdata.test_parameters.film_list_params import film_id_params, genre_para
                           *combined_params
                           ])
 @pytest.mark.asyncio
-async def test_film_list(make_get_request, query_params: dict, expected_data: list,
+async def test_film_list(make_get_request, query_params: dict, expected_data_file: str,
                          status: int, page_size: int):
     """
     Проверка запроса списка фильмов с передачей параметров
@@ -25,16 +29,17 @@ async def test_film_list(make_get_request, query_params: dict, expected_data: li
     # Проверка статуса ответа
     assert response.status == status
 
-    if expected_data is not None:
+    if expected_data_file is not None:
         # Проверка соответствия ответа содержимому файла
+        expected_data = get_data_from_file(files_dir, expected_data_file)
         assert len(response.body) == page_size
         assert response.body == expected_data
 
 
-@pytest.mark.parametrize("film_id, expected_data, status",
+@pytest.mark.parametrize("film_id, expected_data_file, status",
                          [*film_id_params])
 @pytest.mark.asyncio
-async def test_film_by_id(make_get_request, film_id: str, expected_data: dict, status: int):
+async def test_film_by_id(make_get_request, film_id: str, expected_data_file: str, status: int):
     """
     Проверка поиска фильма по id
     """
@@ -43,6 +48,7 @@ async def test_film_by_id(make_get_request, film_id: str, expected_data: dict, s
     # Проверка статуса ответа
     assert response.status == status
 
-    if expected_data is not None:
+    if expected_data_file is not None:
+        expected_data = get_data_from_file(files_dir, expected_data_file)
         # Проверка соответствия ответа содержимому файла
         assert response.body == expected_data
